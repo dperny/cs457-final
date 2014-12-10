8a5902b00bef05d7b424469df6f62ec5354a1ca9
Drew Erny <drewerny@gmail.com>
2014-12-10 00:51:34 -0600

    fixed some bugs in the transmogrifier

--- transmogrifier.rb
+++ transmogrifier.rb
@@ -1,5 +1,6 @@
 # transmogrifier.rb
 require './useless'
+require 'pp'
 
 class Transmogrifier
 
@@ -9,12 +10,15 @@ class Transmogrifier
 
   def self.read(filename)
     contents = []
+    id = 1
     File.open(filename) do |file|
       file.readlines.each do |line|
         record = {}
+        record["ID"] = id.to_s
         line.split(" ").each_slice(2) do |field|
           record[field[0].chomp(":")] = field[1]
         end
+        id += 1
         contents << record
       end
     end
@@ -31,20 +35,25 @@ class Transmogrifier
   end
 
   def self.make_arg_array(raw_args)
-    output = raw_args.gsub(/[\{]|[\}]/,"").split(",").each { |s| s.strip! }
+    output1 = raw_args.gsub(/[\{]|[\}]/,"").split(",").each { |s| s.strip! }
+    output = []
+    output1.each { |o| unless o.empty? then output << o end }
+    output
   end
 
   def self.eval(db, command)
     command = command.split(".")[2]
     method = command.split("(")[0]
-    args = command.split("(")[1].chomp(")").split(",")
+    args = command.split("(")[1].chomp(")")
     rval = if method == "find"
+      args = args.split("},")
       db.find(self.make_arg_hash(args[0]),self.make_arg_array(args[1]))
     elsif method == "count"
-      db.count(args[0])
+      db.count(args)
     elsif method == "min"
-      db.min(args[0])
+      db.min(args)
     elsif method == "cartprod"
+      args = args.split(",").each { |s| s.strip! }
       db.cartprod(args[0],args[1])
     else
       [{}]
